ef30880f38666210298797426fcbb837d068f745
Ankush.Verman <Ankush.Verman@DEV-AnkushV.as.domcorp.zetainteractive.com>
2017-09-13 19:58:42 +0530

    Initial Commit

---
+++ src/main/java/com/rudra/aks/customer/dao/PersonDAO.java
@@ -0,0 +1,12 @@
+package com.rudra.aks.customer.dao;
+
+import java.util.List;
+
+import com.rudra.aks.filter.bo.PersonBO;
+
+public interface PersonDAO {
+	int save(PersonBO customer);
+	List<PersonBO>	search(String columnName, String searchText);
+	void delete(PersonBO customer);
+	PersonBO getPerson(int personid, String defaultUser);
+}
---
+++ src/main/java/com/rudra/aks/customer/service/PersonService.java
@@ -0,0 +1,12 @@
+package com.rudra.aks.customer.service;
+
+import java.util.List;
+
+import com.rudra.aks.filter.bo.PersonBO;
+
+public interface PersonService {
+	int save(PersonBO person);
+	List<PersonBO>	search(String columnName, String searchText);
+	void delete(PersonBO person);
+	PersonBO getPerson(int personid, String defaultUser);
+}
